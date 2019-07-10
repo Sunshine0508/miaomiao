4adc5739c9f9356b376127066a1eedd067c50998
Sunshine <1171626250@qq.com>
2019-07-10 11:31:56 +0800

    bug clear

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -10,7 +10,7 @@
     <div v-else id="content" class="contentDetail">
 
       <div class="detail_list">
-        <div class="detail_list_bg"></div>
+        <div class="detail_list_bg" :style="{'background-image':'url('+detailMovie.img.replace(/w\.h/,'148.208')+')'}"></div>
         <div class="detail_list_filter"></div>
         <div class="detail_list_content">
           <div class="detail_list_img">
@@ -42,7 +42,6 @@
           </li>
         </ul>
       </div>
-
     </div>
 
     <!-- <div id="content" class="contentDetail">
